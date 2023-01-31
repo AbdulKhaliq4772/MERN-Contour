4ad9ef71a66ccebb7886c26a57eb2a3d2d2c0911
abdul khaliq <ak704047@gmail.com>
2023-01-31 15:00:04 +0500

    30-1-23 updated

--- 30-Jan-23 - React 01/Reacts Fundamental/Assignment/script.js
+++ 30-Jan-23 - React 01/Reacts Fundamental/Assignment/script.js
@@ -1,5 +1,12 @@
-let btn = document.querySelector('.increment');
-let msg = document.querySelector('.incrementValue');
+let root = document.querySelector(".root");
+
+let btn = document.createElement("button");
+btn.innerText="increment";
+root.appendChild(btn);
+
+let msg = document.createElement("p");
+msg.innerHTML="";
+root.appendChild(msg);
 
 let count = 0;
 btn.addEventListener('click', ()=>{
